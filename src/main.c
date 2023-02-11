912d4120bda80f223d0aaf17103470a10323e5b4
Noname Kamisawa <noname@kamisawa.net>
2023-02-11 22:29:37 +0900

    ピン配置設定

--- src/main.c
+++ src/main.c
@@ -1,12 +1,40 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED0_NODE				DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0});
+
+static int led1_value = GPIO_ACTIVE_LOW;
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	if (!device_is_ready(led0.port))
+	{
+		printk("led0 is not ready");
+		return;
+	}
+
+	int ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0)
+	{
+		printk("gpio_pin_configure_dt() return code: %d", ret);
+		return;
+	}
+
+	while (1)
+	{
+		led1_value = led1_value == GPIO_ACTIVE_HIGH ? GPIO_ACTIVE_LOW : GPIO_ACTIVE_HIGH;
+		ret = gpio_pin_set_dt(&led0, !led1_value);
+
+		if (ret < 0)
+		{
+			printk("gpio_pin_set_dt() return code: %d", ret);
+			return;
+		}
+
+		k_msleep(100);
+	}
 }
