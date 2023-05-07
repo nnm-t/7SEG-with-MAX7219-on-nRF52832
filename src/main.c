7735c0f92f114444e407616537686353fdcb28e0
Noname Kamisawa <noname@kamisawa.net>
2023-05-07 18:19:07 +0900

    add LED

--- src/main.c
+++ src/main.c
@@ -5,10 +5,14 @@
 #include "max7219.h"
 
 #define LED0_NODE				DT_ALIAS(led0)
+#define LED2_NODE				DT_ALIAS(led2)
+#define LED3_NODE				DT_ALIAS(led3)
 #define SPI0_NODE				DT_NODELABEL(my_spi0)
 #define SPI0_SS_NODE			DT_ALIAS(myspi0ss)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 static const struct gpio_dt_spec spi0_ss = GPIO_DT_SPEC_GET(SPI0_SS_NODE, gpios);
 
 static struct spi_config spi_config = {
@@ -31,16 +35,40 @@ static struct max7219_data max7219_data = {
 
 void main(void)
 {
+	int32_t ret;
+
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	// GPIO
+	if (!device_is_ready(led2.port))
+	{
+		printk("LED2 GPIO is not ready\n");
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0)
+	{
+		printk("LED2 GPIO configure error: %d\n", ret);
+		return;
+	}
+
+	ret = gpio_pin_set_dt(&led2, GPIO_ACTIVE_HIGH);
+
+	if (ret < 0)
+	{
+		printk("LED2 GPIO set error: %d\n", ret);
+		return;
+	}
 
 	// MAX7219
 	// device 取得
 	max7219_config.device_spi = DEVICE_DT_GET(SPI0_NODE);
 
-	int ret = max7219_init(&max7219_config);
+	ret = max7219_init(&max7219_config);
 	if (ret < 0)
 	{
-		printk("MAX7219 init error: %d", ret);
+		printk("MAX7219 init error: %d\n", ret);
 		return;
 	}
 
